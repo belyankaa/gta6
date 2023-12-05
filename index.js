27a75e89e445a22d3cd1d928d39b7517981470e4
belyankaa <nbelogurov.04@bk.ru>
2023-12-05 15:54:52 +0700

    add promocodes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {codes} from "./scripts/config.js";
+
 const close = document.getElementById('close');
 
 const actionBlock = document.getElementById('block');
@@ -5,20 +7,63 @@ const menu = document.getElementById('menu');
 
 const testButtons = document.getElementsByClassName('openBtn');
 
+const input = document.getElementById('input');
+const checkPromoCodeButton = document.getElementById('download');
+const error = document.getElementById('error');
+
 close.onclick = () => {
+    closePromoCodeSpace();
+}
+
+for (let i = 0; i < testButtons.length; i++) {
+    testButtons[i].onclick = () => {
+        openPromoCodeSpace();
+    }
+}
+
+checkPromoCodeButton.onclick = () => {
+    checkPromoCode(input.value);
+}
+
+
+
+
+function download(url) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = url.split('/').pop();
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+function checkPromoCode(code) {
+    error.style.display = 'none';
+    const index = codes.findIndex(item => item.code === code);
+
+    input.value = '';
+
+    if (index === -1) {
+        error.style.display = 'block';
+        return;
+    }
+
+    closePromoCodeSpace();
+    download(codes[index].url);
+}
+
+function openPromoCodeSpace() {
+    close.style.display = 'block';
+    menu.style.display = 'block';
+    actionBlock.classList.remove('closepop');
+    actionBlock.classList.add('open');
+}
+
+function closePromoCodeSpace() {
     close.style.display = 'none';
     actionBlock.classList.add('closepop');
     actionBlock.classList.remove('open');
     setTimeout(() => {
         menu.style.display = 'none';
     }, 300)
-}
-
-for (let i = 0; i < testButtons.length; i++) {
-    testButtons[i].onclick = () => {
-        close.style.display = 'block';
-        menu.style.display = 'block';
-        actionBlock.classList.remove('closepop');
-        actionBlock.classList.add('open');
-    }
 }
---
+++ scripts/config.js
@@ -0,0 +1,14 @@
+export const codes = [
+    {
+        code: 'gta6',
+        url: 'assets/download/1.jpg',
+    },
+    {
+        code: 'gta5',
+        url: 'assets/download/2.jpg'
+    },
+    {
+        code: 'gta4',
+        url: 'assets/download/3.jpg'
+    },
+]
